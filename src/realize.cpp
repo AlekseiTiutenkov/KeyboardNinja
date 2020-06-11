71dc4ef7ec7b9ab3108471399342b58e4b8078d4
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-12 00:20:35 +0300

    Added crushing offers in realize

--- src/realize.cpp
+++ src/realize.cpp
@@ -1,3 +1,29 @@
-void Realize() {
-	
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+
+void realize (char Sentence[])
+{
+    char delim[] = " \t\n,.-";
+    char string[100];
+    char ASentence[100];
+
+    strcpy(ASentence, Sentence);
+    printf("%s \n", Sentence);
+    char* p = strtok(Sentence, delim);
+
+    while (p != NULL) {
+        printf("%s\n", p);
+        scanf("%s", string);
+        p = strtok(NULL, delim);
+        system("CLS");
+        printf("%s \n", ASentence);
+    }
+    system("CLS");
 }
+
+
+
