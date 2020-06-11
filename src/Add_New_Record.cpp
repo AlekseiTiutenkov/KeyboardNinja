3be0cd1e52f57fce55b9880e9fd1c1517599aa97
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-12 01:25:56 +0300

    Add to the function of saving records in a text file

--- src/Add_New_Record.cpp
+++ src/Add_New_Record.cpp
@@ -1,10 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+
 struct Player{
 	float data;
 	char name[20];
 	float time_man;
 }Player1; 
 
-int Add_New_Record() {
+FILE *file, *update_file;
+
+int Add_New_Record(float score, float time_all) {
+	char string[20] = {'\0'};
 	
+	printf("\nEnter your name: ");
+	scanf("%s", string);
+	file = fopen("Records.txt", "a+");
+	if (file == NULL) {
+        printf("Error opening file");
+        getch();
+        return 1;
+    }
+	Player1.data = score;
+	Player1.time_man = time_all;
+	strcpy(Player1.name, string);
+    fwrite(&Player1, sizeof(Player1) , 1 , file);
+    fclose(file);
 	return 0;
 }
+
