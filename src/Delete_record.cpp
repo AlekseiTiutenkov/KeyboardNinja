6387f02729b90b66ad2979b73dd57ecc99f965d5
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2020-06-12 02:34:39 +0700

    Add to the procedure deleting a record in a file by name

--- src/Delete_record.cpp
+++ src/Delete_record.cpp
@@ -1,1 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
 
+void Delete_record(){
+	char update_name[20];
+	system("CLS");
+	update_file = fopen("new.txt" ,"wb");
+  	file = fopen("Records.txt" ,"rb");
+	while (fread(&Player1,sizeof(Player1),1,file))
+     {
+    	fwrite(&Player1, sizeof(Player1),1, update_file);
+      }
+    fclose(update_file);
+    fclose(file);
+    file=fopen("Records.txt" ,"wb");
+    update_file=fopen("new.txt" ,"rb");
+    puts("Enter name: ");
+    scanf("%s", &update_name);
+    while (fread(&Player1,sizeof(Player1),1,update_file)){
+    	if(strcmp(Player1.name,update_name)!=0)
+    		fwrite(&Player1, sizeof(Player1),1, file);
+	}		
+	fclose(file);
+	fclose(update_file);
+}
