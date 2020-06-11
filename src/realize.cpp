a77c0e61793d77d8d3f492682668c3856cd4ef9d
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-12 00:26:51 +0300

    Add validation of the entered word and score in realize

--- src/realize.cpp
+++ src/realize.cpp
@@ -9,7 +9,10 @@ void realize (char Sentence[])
 {
     char delim[] = " \t\n,.-";
     char string[100];
+    int len = 0;
     char ASentence[100];
+   	int k = 0, words = 0;
+	float score = 0;
 
     strcpy(ASentence, Sentence);
     printf("%s \n", Sentence);
@@ -17,13 +20,27 @@ void realize (char Sentence[])
 
     while (p != NULL) {
         printf("%s\n", p);
+        
         scanf("%s", string);
+        
+        if(strcmp(string, p) == 0){
+			len = strlen(string);
+			score+=len;
+			++words;
+			
+		} else {
+			len = strlen(p);
+			score-=len;
+			--k;
+		}
+		
         p = strtok(NULL, delim);
         system("CLS");
         printf("%s \n", ASentence);
     }
-    system("CLS");
+    printf("%f", score);
 }
+
 
 
 
