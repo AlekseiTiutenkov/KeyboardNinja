d717943db3cc14b84c5c2c48be015cb26bfce0fe
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-12 02:01:22 +0300

    Add procedure interface and one case

--- src/Calling_Sentence.cpp
+++ src/Calling_Sentence.cpp
@@ -1,3 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
 struct Lvl{
 	public:
 		char Easy_Level[1000];
@@ -7,5 +13,28 @@ struct Lvl{
 }Levels;
 
 void Calling_Sentence(){
+	char num;
+	bool transition = true;
+	while(transition){
+	printf("Choose difficulty level");
+	printf("\n-----------------------------------------------------");
+	printf("\nEnter 1 to choose easy level.\nEnter 2 to choose medium level.\nEnter 3 to choose hard level.\nEnter 4 to exit.\n");
+	printf("-----------------------------------------------------\n");
+	scanf("%s", &num);
+	num -=48;
+	switch(num){
+				case 1:
+					 system("CLS");	
+					 strcpy(Levels.Easy_Level, "Apart from the cost, shared flats and houses are often in poor condition.\nLandlords are slow to spend their profits on repairs.\n");									
+					 break;
+				default: 
+					printf("Enter the correct value");
+					Sleep(2000);
+					system("CLS");
+					break;
+			
+			}		
+	}
 	
 }
+
