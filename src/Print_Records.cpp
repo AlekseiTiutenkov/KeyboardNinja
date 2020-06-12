aefb23aebbf9ae671c5397e9492a5fd3e0cb9058
Danil Korolenko <d_korolenko15@icloud.com>
2020-06-13 01:22:37 +0800

    Add viewing records in the file in 'Print_Records

--- src/Print_Records.cpp
+++ src/Print_Records.cpp
@@ -1,2 +1,6 @@
 void Print_Records() {
+	file = fopen("Records.txt", "rb");
+    while(fread(& Player1, sizeof(Player1) , 1, file)){
+        printf("\n Name: %12s Score: %6.2f Time: %6.2f", Player1.name, Player1.data, Player1.time_man);
+    }
 }
