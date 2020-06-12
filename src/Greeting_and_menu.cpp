1589b6910d7f51273316844d7c84d5141abca143
Danil Korolenko <d_korolenko15@icloud.com>
2020-06-13 02:24:15 +0800

    Add item for viewing records from a file, deleting records from a file and exit the program

--- src/Greeting_and_menu.cpp
+++ src/Greeting_and_menu.cpp
@@ -17,7 +17,7 @@ int Greeting_and_menu(){
         SetConsoleTextAttribute(hConsole, (WORD) (( 0 << 4) | 4));
         printf("\nEnter 0 for game rules.");
         SetConsoleTextAttribute(hConsole, (WORD) (( 0 << 4) | 9));
-		printf("\nEnter 1 to start.\n");
+		printf("\nEnter 1 to start.\nEnter 2 to display score.\nEnter 3 to delete record\nEnter 4 to exit.\n");
 		printf("\n-----------------------------------------------------\n");
         scanf("%s", &num);
         num -= 48;
@@ -44,6 +44,29 @@ int Greeting_and_menu(){
                 system("CLS");
                 Calling_Sentence();
                 break;
+            case 2:
+                system("CLS");
+                printf("     <<<RECORDS>>>\n");
+                Print_Records();
+                SetConsoleTextAttribute(hConsole, (WORD) (( 0 << 4) | 4));
+                printf("\n\n\n-----------------------------------\n");
+                printf("If you want to exit, press any key|");
+                printf("\n-----------------------------------\n");
+                if(getch()){
+                    SetConsoleTextAttribute(hConsole, (WORD) (( 0 << 4) | 9));
+                    fclose(file);
+                    system("CLS");
+                    break;
+                }
+            case 3:
+                Delete_record();
+                break;
+            case 4:
+                system("CLS");
+                printf("\n\n\n\n\n\n\n\t\t\tGood luck and see you soon\n\n\n\n\n");
+                Sleep(3000);
+                SetConsoleTextAttribute(hConsole, (WORD) (( 0 << 4) | 7));
+                return 0;
 			default: 
                 printf("Enter the correct value");
                 Sleep(2000);
