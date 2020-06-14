01067f19fd4e880ad3c583bea1b179df6f7b544e
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2020-06-15 01:41:17 +0700

    Create main.hpp file

---
+++ src/main.hpp
@@ -0,0 +1,21 @@
+#ifndef main_HPP
+#define main_HPP
+
+typedef struct{
+	float data;
+	char name[20];
+	float time_man;
+}Player; 
+
+
+
+typedef struct{
+	public:
+		char Easy_Level[1000];
+		char Medium_Level[1000];
+		char Hard_Level[1000];
+		
+}Lvl;
+
+
+#endif
