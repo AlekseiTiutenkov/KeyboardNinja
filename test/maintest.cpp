d10397fc2436cdca80695e95606a4b7e530de39f
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-18 03:02:14 +0300

    Create first test for score value

--- test/maintest.cpp
+++ test/maintest.cpp
@@ -1,6 +1,10 @@
 #define CATCH_CONFIG_MAIN
 #include "../thirdparty/catch.hpp"
+#include "../src/Realize.hpp"
 
-TEST_CASE() {
-	REQUIRE(1 == 1);
+TEST_CASE("Example") {
+	char Sentence[] = "Test";
+	float expected = 4;
+	
+	REQUIRE(realize(Sentence) == expected);
 }
