f87bbf8ef39cb284f49e685cdd53e515b730e623
AlekseiTiutenkov@mail.ru <AlekseiTiutenkov@mail.ru>
2020-06-19 02:04:59 +0700

    Сreated a copied version of the Greeting_and_menu function for tests

---
+++ src/Greeting_and_menu_test.cpp
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <windows.h>
+#include "Calling_Sentence.hpp"
+#include "Print_Records.hpp"
+#include "Delete_record.hpp"
+#include "main.hpp"
+
+Player Player1;
+Lvl Levels;
+
+FILE *file, *update_file;
+
+
+int Greeting_and_menu(){
+    char num;
+    int error = 1;
+    while(1){
+      
+        num = 'd';
+        num -= 48;
+        switch(num){
+            case 0:
+                system("CLS");
+                printf("\t\t\t\t<<<<<<RULES OF THE GAME>>>>>>");
+                printf("\n\n\n1)Enter words as quickly as possible.\n2)For correctly typed words you get points.");
+                printf("\n3)For misspelled words you are taken away points.\n4)For misspelled words you are taken away points.");
+                printf("\n5)For all correctly written words, you get +25 points.\n6)If you met the deadline and your score was greater than 0, then your score is increased by a certain factor.");
+                printf("\n7)Throughout the workout you can observe your score, the time of the last word typed and the total time of the set of words.");
+                printf("\n8)At the end of the game you can see your training result and score.\n9)You can delete any of the records by selecting the corresponding menu item..");
+                printf("\n\n\n-----------------------------------\n");
+                printf("If you want to exit, press any key|");
+                printf("\n-----------------------------------\n");
+                if(getch()){
+                    system("CLS");
+                    break;
+                }
+            case 1:
+                system("CLS");
+                Calling_Sentence();
+                break;
+            case 2:
+                system("CLS");
+                printf("     <<<RECORDS>>>\n");
+                Print_Records();
+                printf("\n\n\n-----------------------------------\n");
+                printf("If you want to exit, press any key|");
+                printf("\n-----------------------------------\n");
+                if(getch()){
+                    system("CLS");
+                    break;
+                }
+            case 3:
+                Delete_record();
+                break;
+            case 4:
+                system("CLS");
+                printf("\n\n\n\n\n\n\n\t\t\tGood luck and see you soon\n\n\n\n\n");
+                Sleep(3000);
+                return 0;
+			default: 
+                //printf("Enter the correct value");
+                return error;
+                Sleep(2000);
+                system("CLS");
+                break;    
+		}
+	}
+}
+
---
+++ src/Greeting_and_menu_test.hpp
@@ -0,0 +1,6 @@
+#ifndef Greeting_and_menu_test_HPP
+#define Greeting_and_menu_test_HPP
+
+int Greeting_and_menu();
+
+#endif
