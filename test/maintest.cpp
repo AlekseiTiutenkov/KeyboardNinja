659b67dfba973003cafc2ecd048a7aad9be5f2c2
NikitaKorn <nikitka.kornev.2001@mail.ru>
2020-06-19 00:48:43 +0300

    Add new tests for realize.cpp

--- test/maintest.cpp
+++ test/maintest.cpp
@@ -1,10 +1,45 @@
 #define CATCH_CONFIG_MAIN
 #include "../thirdparty/catch.hpp"
-#include "../src/Realize.hpp"
+#include "../src/Realize_test.hpp"
 
 TEST_CASE("Example") {
 	char Sentence[] = "Test";
 	float expected = 4;
+	
+	REQUIRE(realize(Sentence) == expected);
+}
+
+TEST_CASE("Test: 1 long word") {
+	char Sentence[] = "Dooooooooor";
+	float expected = 11;
+	
+	REQUIRE(realize(Sentence) == expected);
+}
+
+TEST_CASE("Test: empty word") {
+	char Sentence[] = " ";
+	float expected = 0;
+	
+	REQUIRE(realize(Sentence) == expected);
+}
+
+TEST_CASE("Test: sentence 3 words") {
+	char Sentence[] = "One, two, free.";
+	float expected = 10;
+	
+	REQUIRE(realize(Sentence) == expected);
+}
+
+TEST_CASE("Test: long sentence with '.' and '-' ") {
+	char Sentence[] = "One, two, free, four, five, six. eight. nine-ten";
+	float expected = 33;
+	
+	REQUIRE(realize(Sentence) == expected);
+}
+
+TEST_CASE("Test: razdeliteli") {
+	char Sentence[] = "---...";
+	float expected = 0;
 	
 	REQUIRE(realize(Sentence) == expected);
 }
